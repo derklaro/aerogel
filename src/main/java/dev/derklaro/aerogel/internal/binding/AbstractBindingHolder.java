4286155c1b36705da0b882df92ba95fcc9b616a5
derklaro <git@derklaro.dev>
2022-12-02 08:39:21 +0100

    fix: register binding for target type as well

--- src/main/java/dev/derklaro/aerogel/internal/binding/AbstractBindingHolder.java
+++ src/main/java/dev/derklaro/aerogel/internal/binding/AbstractBindingHolder.java
@@ -48,7 +48,6 @@ import org.jetbrains.annotations.Nullable;
 public abstract class AbstractBindingHolder implements BindingHolder {
 
   protected final Injector injector;
-  protected final Element[] targetType;
   protected final Element bindingType;
   protected final Element[] constructedElements;
 
@@ -60,7 +59,6 @@ public abstract class AbstractBindingHolder implements BindingHolder {
    * @param injector the injector to which this binding was bound.
    */
   public AbstractBindingHolder(@NotNull Element[] type, @NotNull Element binding, @NotNull Injector injector) {
-    this.targetType = Objects.requireNonNull(type, "Target type is required to construct");
     this.bindingType = Objects.requireNonNull(binding, "Binding type is required to construct");
     this.injector = Objects.requireNonNull(injector, "The parent injector is required to construct");
 
@@ -83,7 +81,7 @@ public abstract class AbstractBindingHolder implements BindingHolder {
    */
   @Override
   public @NotNull Element[] types() {
-    return this.targetType;
+    return this.constructedElements;
   }
 
   /**
