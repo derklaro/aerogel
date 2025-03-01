8a1fbe911a0f51ceba0ebe674b60a4a002f8522c
Pasqual Koschmieder <git@derklaro.dev>
2025-03-01 10:19:37 +0100

    chore: back to snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
 defaultTasks("build", "test")
 
 allprojects {
-  version = "3.0.0-beta.1"
+  version = "3.0.0-SNAPSHOT"
   group = "dev.derklaro.aerogel"
   description = "A very lightweight jvm dependency injection library"
 
