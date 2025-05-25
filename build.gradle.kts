cb843273a190feab02084fbdcbd2d4e572291ec9
Pasqual Koschmieder <git@derklaro.dev>
2025-05-25 10:39:14 +0200

    chore: prepare for the next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
 defaultTasks("build", "test")
 
 allprojects {
-  version = "3.0.0"
+  version = "3.1.0-SNAPSHOT"
   group = "dev.derklaro.aerogel"
   description = "A very lightweight jvm dependency injection library"
 
