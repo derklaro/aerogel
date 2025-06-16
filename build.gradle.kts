20fa396479ab175b795c1092619fc957bf042911
Pasqual Koschmieder <git@derklaro.dev>
2025-06-16 19:29:46 +0200

    publish: migrate to central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -132,8 +132,8 @@ allprojects {
 nexusPublishing {
   repositories {
     sonatype {
-      nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-      snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+      nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
 
       username.set(rootProject.findProperty("ossrhUsername") as? String ?: "")
       password.set(rootProject.findProperty("ossrhPassword") as? String ?: "")
