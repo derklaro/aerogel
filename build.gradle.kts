a6bafa23e76aaa76d610892544a5b41a2f983a6e
Pasqual Koschmieder <git@derklaro.dev>
2025-10-01 19:16:04 +0200

    release: 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
 defaultTasks("build", "test")
 
 allprojects {
-  version = "3.1.0-SNAPSHOT"
+  version = "3.1.0"
   group = "dev.derklaro.aerogel"
   description = "A very lightweight jvm dependency injection library"
 
